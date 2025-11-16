ceed16c7d1f95f91845b80f8cd650a4bb4dca807
irem <iremyurtoglu07@gmail.com>
2025-11-16 21:51:54 +0300

    update main with loops

--- main.py
+++ main.py
@@ -1,2 +1,753 @@
-print("hello world")
-print("hello world2")
+# print("hello world")
+# print('ali\'nin evi')
+# print("""hello
+# world""")
+# print("hello\nworld")
+# print("hello\tworld")
+# print("hello\t\t\tworld")
+
+# mesaj="hello world"
+# print(mesaj)
+
+# mesaj1="hello"
+# mesaj2="world"
+# print(mesaj1 + mesaj2)
+# print(mesaj1 + " " + mesaj2)
+# print(mesaj1,mesaj2)
+
+# mesaj3="hello"
+# mesaj4="world"
+# print(mesaj3[0])
+# print(mesaj3[1])
+# print(mesaj3[-1])
+# print(mesaj3[-2])
+# print(mesaj3[0:4])
+# print(mesaj3[1:4])
+# print(mesaj3[::1])
+# print(mesaj3[::2])
+# print(mesaj3[::3])
+# print(mesaj3[::4])
+# print(mesaj3[::5])
+# print(mesaj3[::6])
+# print(mesaj3[::7])
+# print(mesaj3[::-1])
+# print(mesaj3[::-2])
+# print(mesaj3[::-3])
+# print(mesaj3[::-4])
+# print(mesaj3[::-5])
+# print(mesaj3[::-6])
+# print(mesaj3[::-7])
+
+# mesaj5="hello"
+# mesaj6="world"
+# print(mesaj5.capitalize())
+# print(mesaj5.casefold())
+# print(mesaj5.center(10))
+# print(mesaj5.center(10,"-"))
+# print(mesaj5.count("1"))
+# print(mesaj5.count("l"))
+# print(mesaj5.encode())
+# print(mesaj5.upper())
+# print(mesaj5)
+# print(mesaj5.startswith("h"))
+# print(mesaj5.startswith("a"))
+# print(mesaj5.endswith("o"))
+# print(mesaj5.endswith("a"))
+
+# mesaj7="hello"
+# mesaj8="world"
+# mesaj7=mesaj7.upper()
+# print(mesaj7)
+# mesaj7=mesaj7.lower()
+# print(mesaj7)
+
+# mesaj9="hello"
+# mesaj10="world"
+# print(len(mesaj9))
+# print(len(mesaj10))
+# print(len(mesaj9 + mesaj10))
+# print(len(mesaj9 + " " + mesaj10))
+
+# print("hello" * 3)
+# print("hello " * 3)
+
+# name= "Ali"
+# age= "21"
+# print("My name is {} and {} years old.".format(name,age))
+# print(f"My name is {name} and {age} years old.")
+
+# number0="10"
+# number1=5
+# number2=3.8
+# print(number0)
+# print(number1)
+# print(number2)
+# print(type(number0))
+# print(type(number1))
+# print(type(number2))
+
+# number3=5 ** 100
+# number4=22 / 7
+# print(number3)
+# print(number4)
+
+# print(2 + 5)
+# print(2 - 5)
+# print(2 * 5)
+# print(2 / 5)
+# print(2 // 5)
+# print(2 % 5)
+# print(2 ** 5)
+# print(abs(-34))
+# print(round(3.5656265))
+# print(round(3.5656365,1))
+# print(round(3.5656365,2))
+# print(3 * 5 + 6)
+# print((3 + 2) * 4 + 3)
+
+# number5=22 / 7
+# print(number5)
+# print(abs(number5))
+# print(round(number5))
+# print(round(number5,1))
+# print(round(number5,2))
+
+# num5= round(3.6565)
+# print(num5)
+
+# print(3 == 3)
+# print(3 == 4)
+# print(3 < 4)
+# print(3 < 2)
+# print(3 > 2)
+# print(3 > 4)
+# print(3 <= 4)
+# print(3 <= 2)
+# print(3 >= 2)
+# print(3 >= 4)
+# print(3 != 4)
+# print(3 !=3)
+
+# number6=7
+# number7=7
+# number8=10
+# number9="100"
+# print(number6 == number7)
+# print(number6 < number8)
+# print(number7 < number8)
+# print(number8 > number6)
+# print(number8 > number7)
+# print(number6 <= number7)
+# print(number6 <= number8)
+# print(number8 >= number6)
+# print(number7 >= number6)
+# print(number6 != number8)
+# print(number6 != number9)
+
+
+# num10=5
+# print(num10)
+
+# num11="5"
+# print(num11)
+
+# num12=int("5")
+# print(num12) 
+
+# number13="5"
+# number14=int(number13)
+# print(number14)
+
+# number15=5
+# number16="5"
+# number17=int(number16)
+# print(number15 == number17)
+
+# number18=123
+# number19=str(number18)
+# print(number19)
+# print(type(number19))
+
+# i= 1
+# i= i + 2
+# print(i)
+
+# i=1
+# i += 2
+# print(i)
+
+# a=5
+# b=10
+# a=b
+# print(a)
+# print(b)
+
+# a=5
+# b=10
+# b=a
+# print(a)
+# print(b)
+
+# colors= ["Black", "White", "Yellow", "Blue", "Green"]
+# print(colors)
+# print(len(colors))
+# print(colors[0])
+# print(colors[1])
+# print(colors[2])
+# print(colors[3])
+# print(colors[4])
+# print(colors[-1])
+# print(colors[-2])
+# print(colors[-3])
+# print(colors[0:])
+# print(colors[1:])
+# print(colors[2:])
+# print(colors[0:4])
+# print(colors[1:4])
+# print(colors[2:4])
+# print(colors[:0])
+# print(colors[:1])
+# print(colors[:2])
+# print(colors[::1])
+# print(colors[::2])
+# print(colors[::3])
+# print(colors[1:4:2])
+
+# colors= ["Black", "White", "Yellow", "Blue", "Green"]
+# print(colors)
+
+
+# colors= ["Black", "White", "Yellow", "Blue", "Green"]
+# colors.append("Red")
+# print(colors)
+
+# colors= ["Black", "White", "Yellow", "Blue", "Green"]
+# colors2= ["Orange", "Pink"]
+# colors.append(colors2)
+# print(colors)
+
+# colors= ["Black", "White", "Yellow", "Blue", "Green"]
+# colors2= ["Orange", "Pink"]
+# colors.extend(colors2)
+# print(colors)
+
+# colors= ["Black", "White", "Yellow", "Blue", "Green"]
+# colors.insert(0,"Red")
+# print(colors)
+
+# colors= ["Black", "White", "Yellow", "Blue", "Green"]
+# colors.insert(1,"Red")
+# print(colors)
+
+# colors= ["Black", "White", "Yellow", "Blue", "Green"]
+# colors.insert(2,"Red")
+# print(colors)
+
+# colors= ["Black", "White", "Yellow", "Blue", "Green"]
+# colors.remove("Yellow")
+# print(colors)
+
+# colors= ["Black", "White", "Yellow", "Blue", "Green"]
+# colors.pop()
+# deleted=colors.pop()
+# print(colors)
+# print(deleted)
+
+# colors= ["Black", "White", "Yellow", "Blue", "Green"]
+# colors.reverse()
+# print(colors)
+
+# colors= ["Black", "White", "Yellow", "Blue", "Green"]
+# colors.sort()
+# print(colors)
+
+# colors= ["Black", "White", "Yellow", "Blue", "Green"]
+# colors.sort(reverse=True)
+# print(colors)
+# colors= ["Black", "White", "Yellow", "Blue", "Green"]
+# list2= sorted(colors)
+# print(colors)
+
+# colors= ["Black", "White", "Yellow", "Blue", "Green"]
+# print(min(colors))
+# print(max(colors))
+
+# colors= ["Black", "White", "Yellow", "Blue", "Green"]
+# for color in colors:
+#     print(color)
+
+# colors= ["Black", "White", "Yellow", "Blue", "Green"]
+# for i in colors:
+#     print(i)
+
+# colors= ["Black", "White", "Yellow", "Blue", "Green"]
+# print("Black" in colors)
+# print("Purple" in colors)
+
+# colors= ["Black", "White", "Yellow", "Blue", "Green"]
+# print(list(enumerate(colors)))
+
+# colors= ["Black", "White", "Yellow", "Blue", "Green"]
+# print(list(enumerate(colors, start=1)))
+
+# colors= ["Black", "White", "Yellow", "Blue", "Green"]
+# print(list(enumerate(colors, start=2)))
+
+# colors= ["Black", "White", "Yellow", "Blue", "Green"]
+# stringcolors= "".join(colors)
+# print(stringcolors)
+
+# colors= ["Black", "White", "Yellow", "Blue", "Green"]
+# stringcolors= " ".join(colors)
+# print(stringcolors)
+
+# colors= ["Black", "White", "Yellow", "Blue", "Green"]
+# stringcolors= ",".join(colors)
+# print(stringcolors)
+
+# colors= ["Black", "White", "Yellow", "Blue", "Green"]
+# stringcolors= "-".join(colors)
+# print(stringcolors)
+# colors3= stringcolors.split("-")
+# print(colors3)
+
+# numbers=[1,2,3,4,5,6,7]
+# print(min(numbers))
+# print(max(numbers))
+# print(sum(numbers))
+
+# tp= ("Yellow", "Blue", "Green", "Red", "Black")
+# print(tp)
+
+# tp= ("Yellow", "Blue", "Green", "Red", "Black")
+# print(type(tp))
+
+# tp= ("Yellow", "Blue", "Green", "Red", "Black")
+# print(len(tp))
+
+# tp= ("Yellow", "Blue", "Green", "Red", "Black")
+# for color in tp:
+#     print(tp)
+
+# set1= {"Yellow", "Blue", "Green", "Red", "Black"}
+# print(set1)
+
+# set1= {"Yellow", "Blue", "Green", "Red", "Black"}
+# print(type(set1))
+
+# set1= {"Yellow", "Blue", "Green", "Red", "Black"}
+# print(len(set1))
+
+# set1= {"Yellow", "Blue", "Green", "Red", "Black"}
+# for color in set1:
+#     print(color)
+
+# set1= {"Yellow", "Blue", "Green", "Red", "Black"}
+# set1.add("Pink")
+# print(set1)
+
+# set1= {"Yellow", "Blue", "Green", "Red", "Black"}
+# set1.remove("Yellow")
+# print(set1)
+
+# set1= {"Yellow", "Blue", "Green", "Red", "Black"}
+# set1.discard("Yellow")
+# print(set1)
+
+# set1= {"Yellow", "Blue", "Green", "Red", "Black"}
+# set1.discard("Purple")
+# print(set1)
+
+# set1= {"Yellow", "Blue", "Green", "Red", "Black"}
+# set2= {"Yellow", "Blue", "Green", "White", "Gray"}
+# print(set1.intersection(set2))
+
+# set1= {"Yellow", "Blue", "Green", "Red", "Black"}
+# set2= {"Yellow", "Blue", "Green", "White", "Gray"}
+# print(set1.union(set2))
+
+# set1= {"Yellow", "Blue", "Green", "Red", "Black"}
+# set2= {"Yellow", "Blue", "Green", "White", "Gray"}
+# print(set1.difference(set2))
+
+# set1= {"Yellow", "Blue", "Green", "Red", "Black"}
+# set2= {"Yellow", "Blue", "Green", "White", "Gray"}
+# print(set2.difference(set1))
+
+# set1= {"Yellow", "Blue", "Green", "Red", "Black"}
+# print("Yellow" in set1)
+
+# set1= {"Yellow", "Blue", "Green", "Red", "Black"}
+# print("White" in set1)
+
+# set1= {"Yellow", "Blue", "Green", "Red", "Black"}
+# set2= {"Yellow", "Blue", "Green", "White", "Gray"}
+# print("Yellow" in set1.union(set2))
+
+# set1= {"Yellow", "Blue", "Green", "Red", "Black"}
+# set2= {"Yellow", "Blue", "Green", "White", "Gray"}  
+# print("Purple" in set1.union(set2))
+
+# set3= set("PYTHON")
+# print(set3)
+
+# set4= set((1,2,3,4,5))
+# print(set4)
+
+# emptylist= []
+# emptylist2= list()
+
+# emptytuple= ()
+# emptytuple2= tuple()
+
+# emptyset= {}
+# print(type(emptyset))
+# emptyset2= set()
+
+# person= {"name": "Ali", "age": 21, "gender": "Male", "hobbies": ["Cinema", "Concert", "Software"]}
+# print(person)
+# print(type(person))
+# print(person["name"])
+# print(person["age"])
+# print(person["gender"])
+# print(person["hobbies"])    
+
+# person= {"name": "Ali", "age": 21, "gender": "Male", "hobbies": ["Cinema", "Concert", "Software"]}
+# person["name"]= "Veli"
+# print(person)
+
+# person= {"name": "Ali", "age": 21, "gender": "Male", "hobbies": ["Cinema", "Concert", "Software"]}
+# person.update({"name": "Veli", "age": 25})
+# print(person)   
+
+# person= {"name": "Ali", "age": 21, "gender": "Male", "hobbies": ["Cinema", "Concert", "Software"]}
+# person["ID"]= 12345
+# print(person)   
+
+# person= {"name": "Ali", "age": 21, "gender": "Male", "hobbies": ["Cinema", "Concert", "Software"]}
+# person["ID"]= 12345
+# print(person)   
+# del person["ID"]
+# print(person)
+
+# person= {"name": "Ali", "age": 21, "gender": "Male", "hobbies": ["Cinema", "Concert", "Software"]}
+# print(person.keys())
+
+# person= {"name": "Ali", "age": 21, "gender": "Male", "hobbies": ["Cinema", "Concert", "Software"]}
+# print(person.values())
+
+# person= {"name": "Ali", "age": 21, "gender": "Male", "hobbies": ["Cinema", "Concert", "Software"]}
+# print(person.items())
+
+# person= {"name": "Ali", "age": 21, "gender": "Male", "hobbies": ["Cinema", "Concert", "Software"]}
+# for x in person:
+#     print(x)
+
+# person= {"name": "Ali", "age": 21, "gender": "Male", "hobbies": ["Cinema", "Concert", "Software"]}
+# for x in person:
+#     print(person[x])
+
+# person= {"name": "Ali", "age": 21, "gender": "Male", "hobbies": ["Cinema", "Concert", "Software"]}
+# for y in person.items():
+#     print(y)
+
+# person= {"name": "Ali", "age": 21, "gender": "Male", "hobbies": ["Cinema", "Concert", "Software"]}
+# for y,z in person.items():
+#     print(y,z)
+
+# person= {"name": "Ali", "age": 21, "gender": "Male", "hobbies": ["Cinema", "Concert", "Software"]}
+# print(person.get("name"))
+# print(person.get("ID"))
+
+# person= {"name": "Ali", "age": 21, "gender": "Male", "hobbies": ["Cinema", "Concert", "Software"]}
+# print(person.get("ID", "Not found"))
+
+# if True:
+#     print("This is true")
+#     print("Still in the if block")
+
+# if False:
+#     print("This is true")
+#     print("Still in the if block")
+
+# a= 5
+# b= 7
+# if a == b:
+#      print("a is equal to b")
+# else:
+#     print("a is not equal to b")
+
+# c= 5
+# d= 7
+# if c != d:
+#      print("c is not equal to d")
+# else:
+#      print("c is equal to d")
+
+# color= "Black"
+# if color == "White":
+#      print("The color is White")
+# elif color == "Yellow":
+#      print("The color is Yellow")
+# elif color == "Blue":
+#      print("The color is Blue")
+# else:
+#      print("None")
+
+# color= "Yellow"
+# if color == "White":
+#      print("The color is White")
+# elif color == "Yellow":
+#      print("The color is Yellow")
+# elif color == "Blue":
+#      print("The color is Blue")
+# else:
+#      print("None")
+
+# x= 5
+# y= 8
+# z= 10
+# if x < y and x < z:
+#      print("Both condiitons are true")
+# else:
+#      print("At least one condiiton is false")
+
+# x= 5
+# y= 8
+# z= 10
+# if x < y or x < z:
+#      print("At least one condition is true")
+# else:
+#      print("Both conditions are false")
+
+# x= 5
+# y= 8
+# z= 10
+# if x < y or x == z:
+#      print("At least one condition is true")
+# else:
+#      print("Both conditions are false")
+
+# x= 5
+# y= 8
+# z= 10
+# if x == y or x == z:
+#      print("At least one condition is true")
+# else:
+#      print("Both conditions are false")
+
+# namee= "Python"
+# l= "P"
+# if l in namee:
+#      print("l is int the namee")
+# else:
+#      print("l is not in the namee")
+
+# namee2= "Python"
+# l2= "a"
+# if not l2 in namee2:
+#      print("l2 is not in the list")
+# else:
+#      print("l2 is in the list")
+
+# list= [1,2,3,4,5,6,7,8,9]
+# k= 4
+# if k in list:
+#      print("k is in the list")
+# else:
+#     print("k is not in the list")
+
+# list2= [1,2,3,4,5,6,7,8,9]
+# k2= 10
+# if not k2 in list2:
+#     print("k2 is not in the list")
+# else:
+#      print("k2 is in the list")
+
+# a = "Python"
+# b = "Pytho"
+# b += "n"
+# print(a)
+# print(b)
+
+# c= "Python"
+# d= "Pytho"
+# d += "n"
+# if a == b:
+#     print("a is equal to b")
+# else:
+#      print("a is not equal to b")
+
+# e= "Python"
+# f= "Pytho"
+# f += "n"
+# if e is f:
+#      print("e is f")
+# else:
+#      print("e is not f")    
+
+# liste= [1,2,3,4,5,6]
+# print(liste[0])
+# print(liste[1])
+# print(liste[2])
+
+# liste= [1,2,3,4,5,6]
+# for number in liste:
+#      print(number)
+
+# isim= "Ali"
+# for letter in isim:
+#     print(letter)
+
+# demet= (1,2,3,4,5,6)
+# for number in demet:
+#      print(number)
+
+# for i in range(10):
+#      print(i)
+
+# for i in range(0,10):
+#      print(i)
+
+# for i in range(1,17,2):
+#      print(i)
+
+# for i in range(10,0, -1):
+#     print(i)
+
+# consult= 1
+# for i in range(0, 10):
+#      consult *= 2
+#      print(consult) 
+
+# consult= 1
+# for i in range(0, 10):
+#      consult *= 2
+# print(consult)
+
+# liste0= range(100)
+# for i in liste0:
+#      if i % 10 == 0:
+#           print(i)
+
+# liste0= range(100)
+# for i in liste0:
+#      if i % 10 != 0:
+#           continue
+#      print(i)
+
+# liste0= range(100)
+# for i in liste0:
+#      if i % 10 != 0:
+#           continue
+#      if i == 50:
+#           break
+#      print(i)
+        
+# liste1= ["a", "b", "c", "d", "e"]
+# liste2= [1,2,3]
+# for letter in liste1:
+#      for number in liste2:
+#         print(letter, number)
+
+# liste3= [1,2,3,4,5,6,7,8,9]
+# for i in liste3:
+#      if i == 3:
+#           continue
+#      print(i)
+
+# liste3= [1,2,3,4,5,6,7,8,9]
+# for i in liste3:
+#      if i == 3:
+#           print("Found 3, skipping it")
+#           continue
+#      print(i)
+
+# liste3= [1,2,3,4,5,6,7,8,9]
+# for i in liste3:
+#      if i == 3:
+#           break
+#      print(i)
+
+# x= 2
+# while x < 10:
+#     print(x)
+#     x += 1
+
+# x= 2
+# y= 3
+# while x * y < 1000:
+#     print(x,y)
+#     x += 2
+#     y += 2
+
+# x= 2
+# while True:
+#     print(x)
+#     x += 2
+#     if x ==100:
+#         break
+
+# i= 1
+# while True:
+#     if i % 2 == 0:
+#         i += 1
+#         continue
+#     print(i)
+#     i += 1
+#     if i == 100:
+#         break
+
+
+
+
+
+
+
+
+
+
+     
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
